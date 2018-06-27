20d6e4171504ccc22f99cfad31b54da997b369ab
Nicholas Arnaud <cobrahead09@gmail.com>
2018-06-26 19:15:51 -0500

    Game for fun

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 endpos;
+    
+    IEnumerator Start()
+    {
+        var pointA = transform.position;
+        while(true)
+        {
+            yield return StartCoroutine(MoveObject(transform, pointA, endpos, 0.50f));
+            yield return StartCoroutine(MoveObject(transform, endpos, pointA, 0.50f));
+        }
+    }
+   
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        var i= 0.0f;
+        var rate= 1.0f/time;
+        while(i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnCoin.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoin : MonoBehaviour
+{
+
+    public GameObject coinToSpawn;
+    private int spawnCount;
+    public float timeToReset = 4;
+    public float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    
+	    timer += Time.deltaTime;
+	    if (Input.touchCount != 0)
+	    {
+	        Vector3 screenToWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+	        if(GetComponent<BoxCollider>() == Physics2D.OverlapPoint(screenToWorld))
+	        {
+	            Instantiate(coinToSpawn,screenToWorld,Quaternion.identity);
+	            spawnCount += 1;
+	            timer = 0;
+	        }
+	    }
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        RaycastHit hit;
+	        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	        if(Physics.Raycast(ray, out hit))
+	        {
+	            if (hit.transform.name == name)
+	            {
+	                if (spawnCount < 6)
+	                {
+	                    var getDistance = Vector3.Distance(ray.origin, hit.transform.position);
+	                    var spawnpoint = ray.GetPoint(getDistance);
+	                    spawnpoint = spawnpoint + Vector3.up/2;
+	                    Instantiate(coinToSpawn,spawnpoint,Quaternion.identity);
+	                    spawnCount += 1;
+	                    timer = 0;
+	                }
+	            }
+	        }
+	    }
+
+	    if (timer >= timeToReset)
+	    {
+	        spawnCount = 0;
+	    }
+	        
+	}
+}
